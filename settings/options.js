4f5accb1103a2f955b2d91ce8b0e10de7571762f
加藤　壮悟 kato.sogo <kato.sogo@fjct.fujitsu.com>
2022-02-14 17:49:30 +0900

    Update project

--- settings/options.js
+++ settings/options.js
@@ -1,23 +1,23 @@
 function saveOptions(e) {
-    e.preventDefault();
-    const enabledGitLabSites = document.querySelector("#enabledGitLabSites").value.replace("\n", ",");
-    browser.storage.sync.set({
-        enabledGitLabSites: enabledGitLabSites,
-    });
+  e.preventDefault();
+  const enabledGitLabSites = document.querySelector("#enabledGitLabSites").value.replace("\n", ",");
+  browser.storage.sync.set({
+    enabledGitLabSites: enabledGitLabSites,
+  });
 }
 
 function restoreOptions() {
 
-    function setCurrentChoice(result) {
-        document.querySelector("#enabledGitLabSites").value = result.enabledGitLabSites.replace(",", "\n");
-    }
+  function setCurrentChoice(result) {
+    document.querySelector("#enabledGitLabSites").value = result.enabledGitLabSites.replace(",", "\n");
+  }
 
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
 
-    const getting = browser.storage.sync.get("enabledGitLabSites");
-    getting.then(setCurrentChoice, onError);
+  const getting = browser.storage.sync.get("enabledGitLabSites");
+  getting.then(setCurrentChoice, onError);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
