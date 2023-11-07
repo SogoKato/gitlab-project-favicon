16f80283a6151d37dc0c90b1f1483d91e803f2ad
Sogo Kato <kato.sogo@fjct.fujitsu.com>
2023-11-07 19:02:08 +0900

    Fix query for gitlab 16.3 or later

--- src/gitlab_project_icon.js
+++ src/gitlab_project_icon.js
@@ -9,13 +9,19 @@
 
     const changeFaviconToProjectLogo = () => {
         const favicon = document.querySelector("link#favicon[rel~='icon']");
-        const img = document.querySelector("*[data-qa-selector='sidebar_menu_link'] img.avatar");
-        const imgV16 = document.querySelector("*[data-qa-selector='context_switcher'] img.gl-avatar");
+        // to 15.11
+        const imgBeforeV16 = document.querySelector("*[data-qa-selector='sidebar_menu_link'] img.avatar");
+        // from 16.0 to 16.3
+        const imgEarlyV16 = document.querySelector(".gl-new-dropdown.context-switcher button img.gl-avatar");
+        // from 16.4
+        const imgLateV16 = document.querySelector("*[data-track-label*='_overview'] img.gl-avatar");
         if (favicon === null) return;
-        if (img) {
-          favicon.href = img.src;
-        } else if (imgV16) {
-          favicon.href = imgV16.src;
+        if (imgBeforeV16) {
+          favicon.href = imgBeforeV16.src;
+        } else if (imgEarlyV16) {
+          favicon.href = imgEarlyV16.src;
+        } else if (imgLateV16) {
+          favicon.href = imgLateV16.src;
         }
     };
 
