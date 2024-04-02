7df5e8670c0b5cca4be9c4c22a8fb37920211b69
Sogo Kato <sogo.kato@outlook.com>
2024-04-02 23:03:43 +0900

    Update script

--- pack_chrome.sh
+++ pack_chrome.sh
@@ -1,6 +1,5 @@
-mkdir ./tmp-chrome
+mkdir -p ./tmp-chrome
 cp -r ./src/* ./tmp-chrome
-sed -i -e 's/"manifest_version": 2/"manifest_version": 3/' ./tmp-chrome/manifest.json
 cp ./tmp-chrome/manifest.json ./tmp-chrome/manifest.tmp.json
-cat ./tmp-chrome/manifest.tmp.json | jq "del(.browser_specific_settings)" > ./tmp-chrome/manifest.json
+cat ./tmp-chrome/manifest.tmp.json | jq ".manifest_version = 3 | .action=.browser_action | del(.browser_specific_settings, .browser_action)" > ./tmp-chrome/manifest.json
 rm ./tmp-chrome/manifest.tmp.json
